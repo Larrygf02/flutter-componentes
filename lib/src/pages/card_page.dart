410de2ad8d8a4180cf1e5849a3f4b309b4161c61
Larrygf02 <raulgf_02@hotmail.com>
2020-08-05 22:10:31 -0500

    Card Widget

---
+++ lib/src/pages/card_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class CardPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cards'),
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(10.0),
+        children: <Widget>[
+          _cardTipo1()
+        ],
+      ),
+    );
+  }
+
+  Widget _cardTipo1() {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            title: Text('Hola soy una tarjeta'),
+            subtitle: Text('Subtitle'),
+            leading: Icon(Icons.photo_album, color: Colors.blue),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              FlatButton(
+                child: Text('Cancelar'),
+                onPressed: () {},
+              ),
+              FlatButton(
+                child: Text('Ok'),
+                onPressed: () {},
+              ),
+            ]
+          )
+        ]
+      ),
+    );
+  }
+}
